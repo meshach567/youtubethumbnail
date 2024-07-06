312fd06d5672d1722c9cade7a3be189025aa298c
meshach567 <arinzemeshach123@gmail.com>
2024-07-06 22:26:41 +0100

    commit tailwind config to ekene branch

---
+++ components/SearchFeed.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Typography, Box } from "@mui/material";
+import { useParams } from "next/navigation";
+
+import  Videos  from '../ui/Videos';
+import FetchApi from "../utils/FetchApi";
+
+function SearchFeed () {
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    FetchApi(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} minHeight="95vh">
+      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
+        Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={videos} />}
+      </Box>
+    </Box>
+  );
+};
+
+export default SearchFeed;
---
+++ ui/Videos.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import ChannelCard from "../components/ChannelCard";
+import Loader from "./Loader";
+import VideoCard from "./VideoCard";
+
+function Videos ({ videos, direction })  {
+  if (!videos?.length) return <Loader />;
+
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
